63742bb321c08e47170987ddae7a228f3073773b
Chris Tetreault <chris@doingmyprogramming.com>
2015-05-09 20:07:28 -0700

    began creating scaffolding for frontend javascript

---
+++ public/javascripts/ctrl_interaction.js
@@ -0,0 +1,109 @@
+/**
+ * Functions to interface with the Controller
+ */
+
+var Controller = function () {
+
+  /**
+   * Join a room
+   * @param roomId = string, The id of the room to join
+   * @return result = {room_name : string, room_id : string, questions : array,
+   * top_questions : array} or {} on failure
+   */
+  var joinRoomImpl = function(roomId) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Create a new room
+   * @param roomName = string, A descriptive name of the room to create
+   * @return result = {room_id : string, room_name : string, owner_id : string}
+   * or {} on failure
+   */
+  var createRoomImpl = function(roomName) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Destroys a room
+   * @param roomId = string, The id of the room to close
+   * @return result = true on success, false on failure
+   */
+  var closeRoomImpl = function(roomId) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Ask a new question
+   * @param questionInfo = {room_id : string, question_text : string}, A room id
+   * to ask the question in, and the text of the question
+   * @return result = The asked question {question_id : string,
+   * question_text : string} or {} on failure
+   */
+  var newQuestionImpl = function(questionInfo) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Upvote a question
+   * @param questionInfo = {room_id : string, question_id : string}, The id of
+   * the room that contains the question to upvote, and the id of the question
+   * to upvote
+   * @return result = The question {room_id : string, question_score : number}
+   * or {} on failure
+   */
+  var upvoteQuestionImpl = function(questionInfo) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Downvote a question
+   * @param questionInfo = {room_id : string, question_id : string}, The id of
+   * the room that contains the question to downvote, and the id of the question
+   * to downvote
+   * @return result = The question {room_id : string, question_score : number}
+   * or {} on failure
+   */
+  var downvoteQuestionImpl = function(questionInfo) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Dismiss a question
+   * @param questionId = string, The id of the question to dismiss
+   * @return result = {question_id : string} the dismissed question id or
+   * {} on failure
+   */
+  var dismissQuestionImpl = function(questionId) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Get the top N questions
+   * @param amount = number, The number of questions to get
+   * @return result = An array of Question objects
+   */
+  var getTopQuestionsImpl = function(amount) {
+    // TODO: Implementation
+  }
+
+  /**
+   * Get all questions
+   * @return result = An array of all Question objects
+   */
+  var getAllQuestionsImpl = function() {
+    // TODO: Implementation
+  }
+
+  return {
+    joinRoom: joinRoomImpl,
+    createRoom: createRoomImpl,
+    closeRoom: closeRoomImpl,
+    newQuestion: newQuestionImpl,
+    upvoteQuestion: upvoteQuestionImpl,
+    downvoteQuestion: downvoteQuestionImpl,
+    dismissQuestion: dismissQuestionImpl,
+    getTopQuestions: getTopQuestionsImpl,
+    getAllQuestions: getAllQuestionsImpl
+    };
+}();
