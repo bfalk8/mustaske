4d7505cec18703d83db67533baff8b26241789c8
Nicholas Gibson <nrgibson@ucsd.edu>
2015-05-09 17:42:24 -0700

    fixed warnUser and banUser, added tests for warnUser, banUser and isBanned, test for addQuestion might not be working...

--- modules/room.js
+++ modules/room.js
@@ -15,21 +15,18 @@ function Room (data) {
 
 /**
  * Increments warnings for user in warnedUsers Hash.
- * calls banUser if warnings == 3
+ * calls banUser if user has already been warned
  *
  * @param data = user_id: id
  * @return number of warnings user has
  */
 Room.prototype.warnUser = function(user_id) {
-  if (!user_id in this.warnedUsers)
-    warnedUsers[user_id] = 1;
+  if (!(user_id in this.warnedUsers))
+    this.warnedUsers[user_id] = true;
   else
-    warnedUsers[user_id] += 1;
-
-  if (warnedUsers[user_id] == 3)
     this.banUser(user_id);
 
-  return warnedUsers[user_id];
+  return this.isBanned(user_id);
 }
 
 /**
@@ -42,7 +39,7 @@ Room.prototype.banUser = function(user_id) {
 
   this.bannedUsers[user_id] = true;
 
-  return (isBanned(user_id));
+  return (this.isBanned(user_id));
 }
 
 /**
--- test/roomtest.js
+++ test/roomtest.js
@@ -7,20 +7,69 @@ var Question = require('../modules/question')
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 
-var room = new Room(
-  {
-    id          : 'testRoom',
-    name        : 'CSE 110',
-    owner       : 'boogeyman'
-  }
-);
-
 var assert = require("assert")
 describe('Room', function(){
+
+  describe('#warnUser()', function(){
+    it('User should be added to warnedUsers hash after first warning,' +
+      'and to bannedUsers hash after second warning.', function(){
+      // Set
+      var room = new Room({room_id: '110', owner_id: 'gary', room_name: 'lecture'});
+      var studentID = 'kyly';
+
+      // Assert
+
+      //no warnings
+      assert(!(studentID in room.warnedUsers));
+      assert(!(studentID in room.bannedUsers));
+
+      room.warnUser(studentID);
+
+      //1 warning
+      assert(studentID in room.warnedUsers);
+      assert(!(studentID in room.bannedUsers));
+
+      room.warnUser(studentID);
+
+      //2 warnings
+      assert(studentID in room.bannedUsers);
+    })
+  })
+  describe('#banUser()', function(){
+    it('User should be added to bannedUsers hash.', function(){
+      // Set
+      var room = new Room({room_id: '110', owner_id: 'gary', room_name: 'lecture'});
+      var studentID = 'kyly';
+
+      // Assert
+      assert(!(studentID in room.bannedUsers));
+      room.banUser(studentID);
+      assert(studentID in room.bannedUsers);
+    })
+  })
+
+  describe('#isBanned()', function(){
+    it('Make sure returns correct values from isBanned hash.', function(){
+      // Set
+      var room = new Room({room_id: '110', owner_id: 'gary', room_name: 'lecture'});
+      var studentID = 'kyly';
+
+      // Assert
+      assert(!room.isBanned(studentID));
+      room.banUser(studentID);
+      assert(room.isBanned(studentID));
+    })
+  })
+
   describe('#addQuestion()', function(){
     it('should have have a Question in questions after addQuestion is called.', function(){
       // Set
+      var roomID = '110';
+      var ownerID = 'gary';
+      var roomName = 'center hall';
 
+      var room = new Room({room_id: roomID, owner_id: ownerID,
+        room_name: roomName});
 
       // Assign
       var newQuestion = {room_id: 'theDopePlace', question_text: 'What is a moustache?', asker_id: 'awesomeStudent'};
