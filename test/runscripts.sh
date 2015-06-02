4485751209249cde2dc186f960a5195eedd0bff4
Kyly <kylyvass@gmail.com>
2015-06-01 23:57:30 -0700

    Fixed test runner to run both set of scripts

--- test/runscripts.sh
+++ test/runscripts.sh
@@ -5,8 +5,9 @@ FILE="test/selenium_src/test_front.log"
 rm -rf $FILE
 
 echo "Results are logged to $FILE"
-
-# npm start
+echo "Stopping other instances of node ./bin/www"
+pkill -f "node ./bin/www"
+node ./bin/www &
 
 # Run all python files in selenium
 for file in $TESTS
@@ -14,3 +15,7 @@ do
   echo "Running $file:" >> $FILE
   python $file 2>&1 | tee -a $FILE
 done
+
+echo "\n\nAll test done"
+echo "Stopping node ./bin/www\n"
+pkill -f "node ./bin/www"
